88339f45cf8d2897fac705fe84f72a9594dc06a0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-04 13:24:51 +0530

    add an Image.asset() widget as a child of the Container() widget inside the build() method of the Ball class

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,8 @@ class Ball extends StatefulWidget {
 class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Image.asset('images/ball1.png'),
+    );
   }
 }
