b2e0898fe2f9584e9ddcf17da0bc8e10735940a0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-04 13:10:34 +0530

    add some reformatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(home: BallPage()),
+      MaterialApp(
+        home: BallPage(),
+      ),
     );
 
 class BallPage extends StatelessWidget {
