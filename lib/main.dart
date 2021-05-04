f97e589cb51fc3efe0859272e4ccf9a9a78b7b16
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-04 13:09:58 +0530

    replace null by class BallPage() in home property of the main() function

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
-      ),
+      MaterialApp(home: BallPage()),
     );
 
 class BallPage extends StatelessWidget {
